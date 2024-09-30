b4a82d0dc8387e1a90c07f7a270948d6e0365ee9
zheanel <53342294+zheanel@users.noreply.github.com>
2024-09-30 16:58:54 +0200

    Updates
    Added logos, updated jQuery to open modals and filled some information

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-jQuery(document).ready(function(){
-    
-})
+jQuery(document).ready(function () {
+  $("#i3-modal-btn").click(function () {
+    $("#i3-modal").modal("show");
+  });
+
+  $("#cs-modal-btn").click(function () {
+    $("#cs-modal").modal("show");
+  });
+
+  $("#ah-modal-btn").click(function () {
+    $("#ah-modal").modal("show");
+  });
+});
