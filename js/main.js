2d9c3421ac0764c5794a4d0229b39db8fe1c8964
skoopeyt <53342294+zheanel@users.noreply.github.com>
2024-10-01 14:15:55 +0200

    Add new functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function () {
+  //Botones que muestran modales de bootstrap
   $("#i3-modal-btn").click(function () {
     $("#i3-modal").modal("show");
   });
@@ -10,4 +11,40 @@ jQuery(document).ready(function () {
   $("#ah-modal-btn").click(function () {
     $("#ah-modal").modal("show");
   });
+
+  //Validacion formulario
+  $(document).ready(function() {
+    $("#contactoWeb").validate({
+        rules: {
+            name: { required: true, minlength: 8 },
+            email: { required: true, email: true },
+            message: { minlength: 10 },
+        },
+        messages: {
+            name: "Este campo es necesario.",
+            email: "Hey! Si no pones tu correo no sabré cómo responderte.",
+            message: "Vaya, ¿te has quedado sin palabras?",
+        }
+    });
+});
+
+//Oculto los elementos al acceder a la web
+
+$("#proyectos").hide();
+$("#contacto").hide();
+
+//Muestro los elementos cuando presiono el boton del navbar
+
+$("#btn-proyectos").click(function () {
+  $("#proyectos").toggle(1000, function() {
+      document.getElementById("proyectos").scrollIntoView();
+  });
+});
+
+$("#btn-contacto").click(function () {
+  $("#contacto").toggle(1000, function() {
+      document.getElementById("contacto").scrollIntoView();
+  });
+});
+
 });
